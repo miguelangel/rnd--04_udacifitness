644bbf0bf3de74b09c885891722e05b652e2b2b6
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-12-02 11:03:33 +0100

    Add basic stepper capabilities

--- components/Steppers.js
+++ components/Steppers.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
+import { FontAwesome } from '@expo/vector-icons';
 
-export default function Steppers () {
-	return (
+const Steppers = ({unit, value, onIncrement, onDecrement}) => (
+	<View>
 		<View>
-			<Text>Steppers</Text>
+			<TouchableOpacity onPress={onDecrement}>
+				<FontAwesome name='minus' size={30} color={'black'} />
+			</TouchableOpacity>
+			<TouchableOpacity onPress={onIncrement}>
+				<FontAwesome name='plus' size={30} color={'black'} />
+			</TouchableOpacity>
 		</View>
-	);
-}
+		<View>
+			<Text>{value}</Text>
+			<Text>{unit}</Text>
+		</View>
+		<Text>Steppers</Text>
+	</View>
+);
+
+Steppers.propTypes = {
+	max: PropTypes.number.isRequired,
+	unit: PropTypes.string.isRequired,
+	step: PropTypes.number.isRequired,
+	value: PropTypes.number.isRequired,
+	onIncrement: PropTypes.func.isRequired,
+	onDecrement: PropTypes.func.isRequired
+};
+
+export default Steppers;
