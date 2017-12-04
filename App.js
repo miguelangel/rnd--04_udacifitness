b349de3140f7b2892c6fa64e48a71187e7c6062f
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-12-04 18:21:30 +0100

    Add custom StatusBar

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
 import React from 'react';
-import {View, Platform} from 'react-native';
+import {View, Platform, StatusBar as NativeStatusBar} from 'react-native';
+import PropTypes from 'prop-types';
 import {TabNavigator} from 'react-navigation';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import {FontAwesome, Ionicons} from '@expo/vector-icons';
+import {Constants} from 'expo';
 import AddEntry from './components/AddEntry';
 import History from './components/History';
 import {purple, white} from './utils/colors';
-import {FontAwesome, Ionicons} from '@expo/vector-icons';
 import reducer from './reducers';
+
+const StatusBar = ({backgroundColor, ... props}) => (
+  <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+    <NativeStatusBar translucent backgroundColor={backgroundColor} {...props}/>
+  </View>
+);
+
+StatusBar.propTypes = {
+  backgroundColor: PropTypes.string.isRequired
+};
 
 const Tabs = TabNavigator({
   History: {
     screen: History,
     navigationOptions: {
       tabBarLabel: 'History',
+      // eslint-disable-next-line react/display-name, react/prop-types
       tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks' size={30}
         color={tintColor}/>
     }
@@ -22,6 +35,7 @@ const Tabs = TabNavigator({
     screen: AddEntry,
     navigationOptions: {
       tabBarLabel: 'Add Entry',
+      // eslint-disable-next-line react/display-name, react/prop-types
       tabBarIcon: ({tintColor}) => <FontAwesome name='plus-square' size={30}
         color={tintColor}/>
     }
@@ -51,7 +65,7 @@ export default class App extends React.Component {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
-          <View style={{height: 20}}/>
+          <StatusBar backgroundColor={purple} barStyle='light-content'/>
           <Tabs/>
         </View>
       </Provider>
