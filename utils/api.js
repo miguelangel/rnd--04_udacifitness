66cbbae1a772e87ce081c1266500ff29157b3587
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-12-04 09:31:43 +0100

    Add fetchCalendarResults method

--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,10 @@
 import { AsyncStorage } from 'react-native';
-import { CALENDAR_STORAGE_KEY }from './_calendar';
+import { CALENDAR_STORAGE_KEY, formatCalendarResults }from './_calendar';
+
+export const fetchCalendarResults = () => (
+	AsyncStorage.getItem(CALENDAR_STORAGE_KEY)
+		.then(formatCalendarResults)
+);
 
 export const submitEntry = ({entry, key}) => (
 	AsyncStorage.mergeItem(CALENDAR_STORAGE_KEY, JSON.stringify({[key]: entry})
