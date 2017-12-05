0e09b9c6ffcfe9d60d629d57f43853a114f5712f
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-12-05 06:31:14 +0100

    Add navigation to home

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
-import { View, TouchableOpacity, Text, Platform,
-  StyleSheet } from 'react-native';
+import React, {Component} from 'react';
+import {View, TouchableOpacity, Text, Platform,
+  StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
-import { Ionicons } from '@expo/vector-icons';
-import { connect } from 'react-redux';
-import { addEntry } from '../actions';
+import {Ionicons} from '@expo/vector-icons';
+import {connect} from 'react-redux';
+import {NavigationActions} from '../react-navigation';
 import Slidder from './Slider';
 import Steppers from './Steppers';
 import DateHeader from './DateHeader';
 import TextButton from './TextButton';
-import { getMetricMetaInfo, timeToString,
-  getDailyReminderValue } from '../utils/helpers';
-import { submitEntry, removeEntry } from '../utils/api';
-import { white, purple} from '../utils/colors';
+import {addEntry} from '../actions';
+import {submitEntry, removeEntry} from '../utils/api';
+import {white, purple} from '../utils/colors';
+import {getMetricMetaInfo, timeToString,
+  getDailyReminderValue} from '../utils/helpers';
 
-const SubmitBtn = ({ onPress }) => (
+const SubmitBtn = ({onPress}) => (
   <TouchableOpacity
     style={Platform.OS === 'ios'
       ? styles.iosSubmitBtn
@@ -31,7 +32,8 @@ SubmitBtn.propTypes = {
 class AddEntry extends Component {
   static propTypes = {
     alreadyLogged: PropTypes.bool,
-    dispatch: PropTypes.func.isRequired
+    dispatch: PropTypes.func.isRequired,
+    navigation: PropTypes.object.isRequired
   };
 
   state = {
@@ -43,7 +45,7 @@ class AddEntry extends Component {
   };
 
   increment = (metric) => {
-    const { max, step } = getMetricMetaInfo(metric);
+    const {max, step} = getMetricMetaInfo(metric);
 
     this.setState((state) => {
       const count = state[metric] + step;
@@ -74,7 +76,6 @@ class AddEntry extends Component {
     const key = timeToString();
     const entry = this.state;
 
-    // TODO: Update redux
     this.props.dispatch(addEntry({
       [key]: entry
     }));
@@ -87,11 +88,9 @@ class AddEntry extends Component {
       eat: 0
     }));
 
-    // TODO: Navigate to home
+    this.toHome();
 
     submitEntry({key, entry});
-
-    // TODO: Clear local notification
   };
 
   reset = () => {
@@ -100,9 +99,14 @@ class AddEntry extends Component {
     this.props.dispatch(addEntry({
       [key]: getDailyReminderValue()
     }));
-    // TODO: Navigate to home
+
+    this.toHome();
 
     removeEntry(key);
+  };
+
+  toHome = () => {
+    this.props.navigation.dispatch(NavigationActions.back({key: 'AddEntry'}));
   };
 
   render() {
@@ -124,14 +128,14 @@ class AddEntry extends Component {
     return (
       <View style={styles.container}>
         <DateHeader date={(new Date()).toLocaleDateString()}/>
-        { Object.keys(metricMetaInfo).map((key) => {
-          const { getIcon, type, ...rest } = metricMetaInfo[key];
+        {Object.keys(metricMetaInfo).map((key) => {
+          const {getIcon, type, ...rest} = metricMetaInfo[key];
           const value = this.state[key];
 
           return (
             <View key={key} style={styles.row}>
-              { getIcon() }
-              { type === 'slider'
+              {getIcon()}
+              {type === 'slider'
                 ? <Slidder
                     value={value}
                     onChange={(value) => this.slide(key, value)}
